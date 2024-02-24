952deee24ac4218f2173bf32d578cae03de43e36
monirshimul <monirshimul@gmail.com>
2024-02-24 16:07:18 +0600

    url changed

--- utils/URL/url.js
+++ utils/URL/url.js
@@ -1,4 +1,5 @@
-export const mainUrl = "http://localhost:8000/";
+// export const mainUrl = "http://localhost:8000/";
+export const mainUrl = "https://web-bio-archive.vercel.app:8000/";
 // export const mainUrl = "https://mock-server-chi.vercel.app/";
 // export const mainUrl = "https://my-json-server.typicode.com/monirshimul/mydb/";
 
