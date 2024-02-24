b2b7e7b49daa3fbc74552a303b44407466df33c4
monirshimul <monirshimul@gmail.com>
2024-02-24 16:14:29 +0600

    URL modified

--- utils/URL/url.js
+++ utils/URL/url.js
@@ -1,7 +1,8 @@
 // export const mainUrl = "http://localhost:8000/";
-export const mainUrl = "https://web-bio-archive.vercel.app:8000/";
+// export const mainUrl = "https://web-bio-archive.vercel.app:8000/";
 // export const mainUrl = "https://mock-server-chi.vercel.app/";
-// export const mainUrl = "https://my-json-server.typicode.com/monirshimul/mydb/";
+export const mainUrl = "https://my-json-server.typicode.com/monirshimul/mydb/";
+export const mainUrlforProducts = "https://dummyjson.com/";
 
 export const certification = `${mainUrl}certification`;
-export const products = `${mainUrl}products`;
+export const products = `${mainUrlforProducts}products`;
